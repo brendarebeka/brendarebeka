ea26f88437f82f1454d17533695009662e543e90
blow me up <brenda.marini@escola.pr.gov.br>
2026-03-30 13:48:58 +0000

    versaosim

--- cnh.js
+++ cnh.js
@@ -1,3 +1,13 @@
-const idade = 15;
+const idade = 16;
 const maiorIdade = idade >= 18;
 const possuiCNH = false;
+
+const podeDigirir = maiorIdade && possuiCNH;
+console.log('Pode viajar sozinho?', podeDigirir);
+
+const podeViajarSozinho = maiorIdade || possuiCNH;
+console.log('Pode viajar sozinho?', podeViajarSozinho);
+
+const precisaAcompanhante = !maiorIdade
+
+console.log('Precisa de acompanhante?', precisaAcompanhante)
