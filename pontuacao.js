8ab25368c6ac26892f721cfa6d1fb6d80db23459
blow me up <brenda.marini@escola.pr.gov.br>
2026-04-07 11:50:07 +0000

    versao2

---
+++ pontuacao.js
@@ -0,0 +1,12 @@
+//Sistema de pontuação de um jogo
+const nomeJogador = "Null"
+let faseAtual = 3;
+let VidasRestantes = 2;
+
+let pontosPorMissao [85, 90, 60,100]
+
+//pontosPorMissao.length
+let pontuacaoTotal = 0;
+
+console.log(`Seja bem vinda ${nomeJogador}!`);
+console.log(pontosPorMissao);
