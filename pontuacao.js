a322ad36ea99f69bc6cc030f97cccd4b6afeb149
blow me up <brenda.marini@escola.pr.gov.br>
2026-04-08 12:31:16 +0000

    versao2

--- pontuacao.js
+++ pontuacao.js
@@ -8,5 +8,14 @@ let pontosPorMissao [85, 90, 60,100]
 //pontosPorMissao.length
 let pontuacaoTotal = 0;
 
+for ( let i = 0; i < pontuacoes.lenght; i++) {
+    pontuacaoTotal = pontuacaoTotal + pontuacoes[i]
+}
+
+//Calculando a média
+let media = pontuacaoTotal / pontuacoes.lenght;
+
+let passouDeFase = (media >= 75 && vidas > 0) ||) (fase === 1);
+
 console.log(`Seja bem vinda ${nomeJogador}!`);
 console.log(pontosPorMissao);
