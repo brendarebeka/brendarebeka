1a62200f8a7f3df009755466a6b40e6034bc60ec
blow me up <brenda.marini@escola.pr.gov.br>
2026-04-15 09:36:37 -0300

    Add files via upload

--- array2.js
+++ array2.js
@@ -1,4 +1,15 @@
-//Lista de nomes
-const estudantes = ["Ana", "Eduarda", "Carla"];
+const alunos = ["Ana", "Bruna", "Carla"];
 
-console.log(estudantes)
+alunos.push("Diego") // Adiciona um elemento no final do aarray
+alunos.unshift("Alan"); //Adiciona elementos no início do array
+console.log(alunos);
+
+const removido = alunos.pop()
+console.log(removido)
+console.log(alunos)
+
+console.log(alunos.indexOf("Carla"));
+console.log(alunos.lastIndexOf("Pedro"));
+
+console.log(alunos.includes(Ana)); //Retorna o valor booleano
+console.log(alunos.includes("Paula"));
